682099baa0af59b69956597a34c8cccc955d19eb
blackingg <mudet04@gmail.com>
2024-10-03 04:54:51 +0100

    #

--- src/components/LoadingDots.jsx
+++ src/components/LoadingDots.jsx
@@ -4,15 +4,15 @@ const LoadingDots = () => {
   return (
     <div className="flex justify-center items-center space-x-2">
       <div
-        className="w-3 h-3 bg-[#ce9267] rounded-full animate-bounce"
+        className="w-3 h-3 bg-[#6F4E37] rounded-full animate-bounce"
         style={{ animationDelay: "0s" }}
       ></div>
       <div
-        className="w-3 h-3 bg-[#77543b] rounded-full animate-bounce"
+        className="w-3 h-3 bg-[#6F4E37] rounded-full animate-bounce"
         style={{ animationDelay: "0.2s" }}
       ></div>
       <div
-        className="w-3 h-3 bg-[#4b3425] rounded-full animate-bounce"
+        className="w-3 h-3 bg-[#6F4E37] rounded-full animate-bounce"
         style={{ animationDelay: "0.4s" }}
       ></div>
     </div>
